46aac098fe9c433c784f78f3a8d26724a1c923ce
o9uzdev <o9uzdev@gmail.com>
2021-06-27 12:00:17 +0300

    Create JavaFx Application

---
+++ src/com/o9uzdev/javaFxClockAnimation/App.java
@@ -0,0 +1,11 @@
+package com.o9uzdev.javaFxClockAnimation;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        // let's code
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module javaFxClockAnimation {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires java.base;
+
+    opens com.o9uzdev.javaFxClockAnimation;
+}
