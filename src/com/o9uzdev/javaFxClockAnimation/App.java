ebdb62930a6f7420e5e34343bb2061a2d55245db
o9uzdev <o9uzdev@gmail.com>
2021-06-29 19:28:28 +0300

    Added label and Scene, modify stage

--- src/com/o9uzdev/javaFxClockAnimation/App.java
+++ src/com/o9uzdev/javaFxClockAnimation/App.java
@@ -1,11 +1,26 @@
 package com.o9uzdev.javaFxClockAnimation;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        // let's code
+        Label lblTime = new Label();
+        lblTime.setFont(new Font(14));
+
+        BorderPane root = new BorderPane();
+        root.setBottom(lblTime);
+        BorderPane.setAlignment(lblTime, Pos.TOP_CENTER);
+
+        Scene scene = new Scene(root,300,300);
+        stage.setScene(scene);
+        stage.setTitle("Clock Animation");
+        stage.show();
     }
 }
